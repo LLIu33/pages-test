9b573ffaf3004c261e741b8aeaa006bf10847120
Artem Grechko <artem@intspirit.com>
2017-05-15 11:03:50 +0300

    Changed dataTable

--- assets/js/controllers/dataTables.js
+++ assets/js/controllers/dataTables.js
@@ -7,21 +7,24 @@ angular
   .controller("AngularUITableCtrl", [
     "$scope",
     function($scope) {
+      var cellWidth = 100;
       $scope.gridOptions = {
         enableSorting: false,
         enableFiltering: false,
         enableCellEditOnFocus: false,
         enableColumnMenu: false,
+        rowHeight: 50,
+        // headerTemplate: '<div class="ui-grid-top-panel" style="text-align: center">I am a Custom Grid Header</div>',
         columnDefs: [
           { name: 'part', width:100},
-          { name: 'Other 2', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100},
-          { name: 'Other 3', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100 },
-          { name: 'Site 4', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100 },
-          { name: 'Other 21', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100 },
-          { name: 'Other 31', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100 },
-          { name: 'Site 2', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100 },
-          { name: 'Other 22', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100},
-          { name: 'Other 32', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:100 }
+          { name: 'Other 2', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth},
+          { name: 'Other 3', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth },
+          { name: 'Site 4', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth },
+          { name: 'Other 21', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth },
+          { name: 'Other 31', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth },
+          { name: 'Site 2', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth },
+          { name: 'Other 22', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth},
+          { name: 'Other 32', cellTemplate: '<div class="circle-{{grid.getCellValue(row,col)}}" ></div>', width:cellWidth }
         ],
         data:[
           {
@@ -37,58 +40,47 @@ angular
           },
           {
             part: "Part 2",
-            "Other 2": "Green",
-            "Other 3": "Red",
+            "Other 2": "Red",
+            "Other 3": "Green",
             "Site 4": "Yellow",
-            "Other 21": "Yellow",
+            "Other 21": "Red",
             "Other 31": "Green",
             "Site 2": "Red",
-            "Other 22": "Red",
-            "Other 32": "Green"
+            "Other 22": "Green",
+            "Other 32": "Yellow"
           },
           {
             part: "Part 3",
-            "Other 2": "Green",
-            "Other 3": "Red",
-            "Site 4": "Yellow",
-            "Other 21": "Yellow",
+            "Other 2": "Red",
+            "Other 3": "Yellow",
+            "Site 4": "Green",
+            "Other 21": "Red",
             "Other 31": "Green",
-            "Site 2": "Red",
-            "Other 22": "Red",
-            "Other 32": "Green"
+            "Site 2": "Green",
+            "Other 22": "Green",
+            "Other 32": "Yellow"
           },
           {
             part: "Part 4",
-            "Other 2": "Green",
-            "Other 3": "Red",
-            "Site 4": "Yellow",
-            "Other 21": "Yellow",
+            "Other 2": "Red",
+            "Other 3": "Green",
+            "Site 4": "Red",
+            "Other 21": "Red",
             "Other 31": "Green",
             "Site 2": "Red",
-            "Other 22": "Red",
-            "Other 32": "Green"
-          },
-          {
-            part: "Part 4",
-            "Other 2": "Green",
-            "Other 3": "Red",
-            "Site 4": "Yellow",
-            "Other 21": "Yellow",
-            "Other 31": "Green",
-            "Site 2": "Red",
-            "Other 22": "Red",
-            "Other 32": "Green"
+            "Other 22": "Green",
+            "Other 32": "Yellow"
           },
           {
             part: "Part 5",
-            "Other 2": "Green",
-            "Other 3": "Red",
-            "Site 4": "Yellow",
-            "Other 21": "Yellow",
+            "Other 2": "Red",
+            "Other 3": "Yellow",
+            "Site 4": "Red",
+            "Other 21": "Red",
             "Other 31": "Green",
-            "Site 2": "Red",
-            "Other 22": "Red",
-            "Other 32": "Green"
+            "Site 2": "Green",
+            "Other 22": "Green",
+            "Other 32": "Yellow"
           },
           {
             part: "Part 6",
@@ -105,221 +97,3 @@ angular
       }
     }
   ])
-
-  // data: [{
-  //     part: "Part 1",
-  //     sites: [{
-  //         site: "Other 2",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Site 4",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Site 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     part: "Part 2",
-  //     sites: [{
-  //         site: "Other 2",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Site 4",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Site 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     part: "Part 3",
-  //     sites: [{
-  //         site: "Other 2",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Site 4",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Site 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     part: "Part 4",
-  //     sites: [{
-  //         site: "Other 2",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Site 4",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Site 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     part: "Part 5",
-  //     sites: [{
-  //         site: "Other 2",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Site 4",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Site 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     part: "Part 6",
-  //     sites: [{
-  //         site: "Other 2",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Site 4",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Yellow"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       },
-  //       {
-  //         site: "Site 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 2",
-  //         status: "Red"
-  //       },
-  //       {
-  //         site: "Other 3",
-  //         status: "Green"
-  //       }
-  //     ]
-  //   }
-  // ];
